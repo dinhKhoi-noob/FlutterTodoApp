6efc77703fd7be6590db5c525d0185c3ad2d3895
dinhKhoi-noob <khoip1305@gmail.com>
2021-11-24 09:48:38 +0700

    Change minSdkVersion

--- lib/services/authenticate.dart
+++ lib/services/authenticate.dart
@@ -1,12 +1,12 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthServices {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+// class AuthServices {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<void> signinAnonymous() async {
-    try {
-      UserCredential result = await _auth.signInAnonymously();
+//   Future<void> signinAnonymous() async {
+//     try {
+//       UserCredential result = await _auth.signInAnonymously();
       
-    } catch (exception) {}
-  }
-}
+//     } catch (exception) {}
+//   }
+// }
