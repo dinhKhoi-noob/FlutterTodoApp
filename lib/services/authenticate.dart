37ac579adfbe919bfca8831655ff37512fb07641
dinhKhoi-noob <khoip1305@gmail.com>
2021-11-24 08:57:01 +0700

    Register firebase

---
+++ lib/services/authenticate.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthServices {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> signinAnonymous() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      
+    } catch (exception) {}
+  }
+}
